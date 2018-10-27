79fca305fa714c2a3c85ec6413e8969d8e81dcbd
Ariel Guzman <ariel.guzman01@gmail.com>
2018-10-27 16:18:19 -0400

    Added comments, added search bar and made the app better

--- src/pages/add-item/add-item.ts
+++ src/pages/add-item/add-item.ts
@@ -13,14 +13,16 @@ export class AddItemPage {
   addOrEdit: string = "Add";
   index: number = 0;
 
+
   constructor(public navCtrl: NavController, public navParams: NavParams, private storage: Storage,
               private viewCtrl: ViewController) {
+    //If the navParams contains "originalValue" it means that the modal was called from the Edit Button"
     if (this.navParams.get("originalValue")) {
       this.description = this.navParams.get("originalValue");
       this.index = this.navParams.get("index");
       this.addOrEdit = "Edit"
     }
-    this.navCtrl.swipeBackEnabled = true;
+    //
     storage.get("items").then(value => {
       if (value != null) {
         this.items = value
@@ -28,16 +30,21 @@ export class AddItemPage {
     })
   }
 
+  /*
+  * Check the current title of the Modal and based on that either creates a new item or modifies the current one*/
   addDescription() {
     if (this.addOrEdit == "Add") {
       this.items.push(this.description);
     } else {
       this.items.splice(this.index, 1, this.description)
     }
-    this.storage.set("items", this.items)
+    this.storage.set("items", this.items);
     this.closeModal()
   }
 
+
+  /*
+  * Closes the modal and notifies the caller*/
   closeModal() {
     this.viewCtrl.dismiss();
   }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,22 +10,38 @@ import {Storage} from "@ionic/storage";
 export class HomePage {
 
   items: string[] = [];
+  staticItems: string[] = [];
+  showSearchBar: boolean = false;
+  searchTerm: string = "";
 
   constructor(public navCtrl: NavController, private modalCtrl: ModalController, private storage: Storage) {
     this.getItems()
   }
 
+  /*
+  * This method opens a new instance of AddItemPage, and from there we create a new Item.
+  * Once the AddItemPage modal is closed we query the data from IndexedDB (if indexedDB is not available, it will use webSQL)
+  * */
   addListItem(){
     const modal = this.modalCtrl.create(AddItemPage);
+    modal.onDidDismiss(data => {
+      this.getItems();
+    });
     modal.present();
   }
 
+  /*
+  * Removes an Item from the Database
+  * */
   removeItem(value: string){
     let index = this.items.indexOf(value);
     this.items.splice(index, 1);
     this.storage.set("items", this.items)
   }
 
+  /*
+  * Gets the current item, open a new instance of AddItemPage and pass the values that it is going to change.
+  * */
   editItem(value: string){
     let index = this.items.indexOf(value);
     const modal = this.modalCtrl.create(AddItemPage, {originalValue: value, index: index});
@@ -35,15 +51,49 @@ export class HomePage {
     modal.present();
   }
 
+  /*
+  * Uses the method that comes with Ionic Item Reorder and overrides the items saved on the database*/
   reorderItems(event){
     event.applyTo(this.items);
     this.storage.set("items", this.items)
   }
 
+  /*
+  * Queries IndexedDB and set the values for the local items array
+  * */
   getItems(){
     this.storage.get("items").then(value => {
-      this.items = value
+      this.items = value;
+      this.staticItems = value;
     })
+  }
+
+  /*
+  * Uses a staticItems variable that is never tampered with, and finds any item that match with the searchTerm
+  * */
+  onSearch(event){
+    this.items = this.staticItems.filter(desc => desc.includes(this.searchTerm));
+  }
+
+  /*
+  * When the onCancel method is called (method provided by the IonSearchBar) it will either show or hide the searchbar,
+   * it will also change the value of the items to the default if is currently being shown */
+  onCancel(event){
+    if (this.showSearchBar){
+      this.showSearchBar = false;
+      this.items = Object.assign([], this.staticItems)
+    } else {
+      this.showSearchBar = true
+    }
+  }
+
+  /*
+  * Removes all values
+  * */
+  clearAll(){
+    this.items = [];
+    this.staticItems = [];
+    this.storage.set("items", []);
   }
 
 }
